0f23711ad6a06ebfa0929ce0833c57225c07acd3
Arthur Gartner <Arthur.Gartner@gmail.com>
2023-07-20 20:28:33 -0600

    Updates to span tag insert

--- scripts/content.js
+++ scripts/content.js
@@ -7,29 +7,54 @@ function replaceVisibleHexValues() {
     if (nodeType === Node.TEXT_NODE) {
       const matches = node.nodeValue.match(hexPattern);
       if (matches) {
-        const spanTag = document.createElement("span");
-        chrome.storage.local.get(["background"]).then((value) => {
-          if (value.background) {
-            chrome.storage.local.get(["backgroundHover"]).then((hoverValue) => {
-              if (hoverValue.backgroundHover) {
-                removeBackgroundColor(spanTag);
-                addBackgroundColorOnHover(spanTag, matches[0]);
-              } else addBackgroundColor(spanTag, matches[0]);
-            });
-          } else removeBackgroundColor(spanTag);
+        // First, let's check if the matched node's parent is a span with class "curlor"
+        // If it is, we remove the span and insert its content directly into the grandparent node.
+        if (
+          node.parentNode.tagName === "SPAN" &&
+          node.parentNode.classList.contains("curlor")
+        ) {
+          const textNode = document.createTextNode(matches[0]);
+          node.parentNode.parentNode.replaceChild(textNode, node.parentNode);
+          node = textNode; // Update node reference for further processing
+        }
+
+        // Now fetch both 'background' and 'color' values in parallel
+        Promise.all([
+          chrome.storage.local.get(["background"]),
+          chrome.storage.local.get(["color"]),
+        ]).then(([backgroundValue, colorValue]) => {
+          if (backgroundValue.background || colorValue.color) {
+            const spanTag = document.createElement("span");
+            spanTag.className = "curlor"; // Setting class to "curlor"
+
+            if (backgroundValue.background) {
+              chrome.storage.local
+                .get(["backgroundHover"])
+                .then((hoverValue) => {
+                  if (hoverValue.backgroundHover) {
+                    removeBackgroundColor(spanTag);
+                    addBackgroundColorOnHover(spanTag, matches[0]);
+                  } else {
+                    addBackgroundColor(spanTag, matches[0]);
+                  }
+                });
+            }
+
+            if (colorValue.color) {
+              chrome.storage.local.get(["colorHover"]).then((hoverValue) => {
+                if (hoverValue.colorHover) {
+                  removeTextColor(spanTag);
+                  addTextColorOnHover(spanTag, matches[0]);
+                } else {
+                  addTextColor(spanTag, matches[0]);
+                }
+              });
+            }
+
+            spanTag.textContent = matches[0];
+            node.parentNode.replaceChild(spanTag, node);
+          }
         });
-        chrome.storage.local.get(["color"]).then((value) => {
-          if (value.color) {
-            chrome.storage.local.get(["colorHover"]).then((hoverValue) => {
-              if (hoverValue.colorHover) {
-                removeTextColor(spanTag);
-                addTextColorOnHover(spanTag, matches[0]);
-              } else addTextColor(spanTag, matches[0]);
-            });
-          } else removeTextColor(spanTag);
-        });
-        spanTag.textContent = matches[0];
-        node.parentNode.replaceChild(spanTag, node);
       }
     } else if (nodeType === Node.ELEMENT_NODE) {
       const style = window.getComputedStyle(node);
