579868c865eb459aa5d9846b288c2dddf2344778
Arthur Gartner <Arthur.Gartner@gmail.com>
2023-07-23 12:44:12 -0600

    Fixed code for multiple color values in same element

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -3,82 +3,104 @@ function replaceVisibleColorValues() {
   const hexrgbhslPattern =
     /#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})\b|(rgb|hsl)\s*\(?\s*(\d{1,3})\s*,\s*(\d{1,3}%?)\s*,\s*(\d{1,3}%?)\s*\)?/g;
   // Traverse DOM structure to find matches
-  function traverse(node) {
-    const nodeType = node.nodeType;
+  function traverse(rootNode) {
+    const nodesToProcess = [rootNode];
 
-    // Ensure node is proper type
-    if (nodeType === Node.TEXT_NODE) {
-      const matches = node.nodeValue.match(hexrgbhslPattern);
-      if (matches) {
-        // Check if previously added span exists, if so remove
-        if (
-          node.parentNode.tagName === "SPAN" &&
-          node.parentNode.classList.contains("curlor")
-        ) {
-          const textNode = document.createTextNode(matches[0]);
-          node.parentNode.parentNode.replaceChild(textNode, node.parentNode);
-          node = textNode; // Update node reference for further processing
-        }
+    // Pre-fetch the necessary values from chrome.storage to avoid repeated async calls.
+    let backgroundValue, colorValue, backgroundHoverValue, colorHoverValue;
 
-        // Assign match to color value
-        var color = matches[0];
+    Promise.all([
+      chrome.storage.local.get(["background"]),
+      chrome.storage.local.get(["color"]),
+      chrome.storage.local.get(["backgroundHover"]),
+      chrome.storage.local.get(["colorHover"]),
+    ]).then(([bgValue, clrValue, bgHoverValue, clrHoverValue]) => {
+      backgroundValue = bgValue;
+      colorValue = clrValue;
+      backgroundHoverValue = bgHoverValue;
+      colorHoverValue = clrHoverValue;
 
-        //If hsl value then conversion to hex necessary
-        if (isHSL(color)) color = convertHSLToHex(color);
+      while (nodesToProcess.length) {
+        const node = nodesToProcess.shift();
+        const nodeType = node.nodeType;
 
-        // Fetch both 'background' and 'color' values in parallel
-        Promise.all([
-          chrome.storage.local.get(["background"]),
-          chrome.storage.local.get(["color"]),
-        ]).then(([backgroundValue, colorValue]) => {
-          if (backgroundValue.background || colorValue.color) {
+        if (nodeType === Node.TEXT_NODE) {
+          let content = node.nodeValue;
+          let offset = 0;
+          let match;
+
+          while ((match = hexrgbhslPattern.exec(content)) !== null) {
+            var color = match[0];
+
+            if (isHSL(color)) {
+              color = convertHSLToHex(color);
+            }
+
             const spanTag = document.createElement("span");
-            spanTag.className = "curlor"; // Setting class to "curlor"
+            spanTag.className = "curlor";
+            spanTag.textContent = color;
 
-            if (backgroundValue.background) {
-              chrome.storage.local
-                .get(["backgroundHover"])
-                .then((hoverValue) => {
-                  if (hoverValue.backgroundHover) {
-                    addBackgroundColorOnHover(spanTag, color);
-                  } else {
-                    addBackgroundColor(spanTag, color);
-                  }
-                });
+            const matchStart = match.index;
+            const matchEnd = match.index + match[0].length;
+
+            if (matchStart > offset) {
+              node.parentNode.insertBefore(
+                document.createTextNode(content.substring(offset, matchStart)),
+                node
+              );
             }
 
-            if (colorValue.color) {
-              chrome.storage.local.get(["colorHover"]).then((hoverValue) => {
-                if (hoverValue.colorHover) {
+            if (backgroundValue.background || colorValue.color) {
+              if (backgroundValue.background) {
+                if (backgroundHoverValue.backgroundHover)
+                  addBackgroundColorOnHover(spanTag, color);
+                else addBackgroundColor(spanTag, color);
+              }
+
+              if (colorValue.color) {
+                if (colorHoverValue.colorHover)
                   addTextColorOnHover(spanTag, color);
-                } else {
-                  addTextColor(spanTag, color);
-                }
-              });
+                else addTextColor(spanTag, color);
+              }
+
+              node.parentNode.insertBefore(spanTag, node);
+            } else {
+              // If no color or background applied, just insert the text without the span
+              node.parentNode.insertBefore(
+                document.createTextNode(color),
+                node
+              );
             }
 
-            // Add the text value to the span tag
-            spanTag.textContent = matches[0];
-
-            // Insert newly created span tag
-            if (node.parentNode != null)
-              node.parentNode.replaceChild(spanTag, node);
+            offset = matchEnd;
           }
-        });
-      }
-    } else if (nodeType === Node.ELEMENT_NODE) {
-      const style = window.getComputedStyle(node);
-      const isHidden =
-        style.visibility === "hidden" || style.display === "none";
 
-      // Continue traversal if visible
-      if (!isHidden) {
-        const childNodes = Array.from(node.childNodes);
-        for (const childNode of childNodes) {
-          traverse(childNode);
+          if (offset < content.length) {
+            node.parentNode.insertBefore(
+              document.createTextNode(content.substring(offset)),
+              node
+            );
+          }
+
+          node.parentNode.removeChild(node);
+        } else if (nodeType === Node.ELEMENT_NODE) {
+          // For toggling off, we should check if the element is a `span.curlor` and replace it with its text content
+          if (node.tagName === "SPAN" && node.classList.contains("curlor")) {
+            const textNode = document.createTextNode(node.textContent);
+            node.parentNode.replaceChild(textNode, node);
+            nodesToProcess.push(textNode); // Add the new textNode to the processing list
+          } else {
+            const style = window.getComputedStyle(node);
+            const isHidden =
+              style.visibility === "hidden" || style.display === "none";
+
+            if (!isHidden) {
+              nodesToProcess.push(...node.childNodes);
+            }
+          }
         }
       }
-    }
+    });
   }
 
   // Start traversal at top
